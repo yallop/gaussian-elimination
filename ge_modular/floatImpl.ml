36b92480ac1d23b9f548faec19fda9caf8bdd4cc
Jacques Carette <carette@mcmaster.ca>
2017-08-24 21:58:29 -0400

    add inv, aka unary division.  Augment FloatStaged with implementations of minus, inv and div.

--- ge_modular/floatImpl.ml
+++ ge_modular/floatImpl.ml
@@ -15,9 +15,10 @@ module FloatCode = struct
   let one       = .<  1. >.
   let negone    = .< -1. >.
   let plus  x y = .<.~x +. .~y>.
-  let times x y = .<.~x *. .~y>.
-  let minus x y = .<.~x -. .~y>.
   let uminus x  = .<-. .~x>.
+  let minus x y = .<.~x -. .~y>.
+  let times x y = .<.~x *. .~y>.
+  let inv x     = .< 1. /. .~x>.
   let div x y   = .<.~x /. .~y>.
   let better_than = Some (fun x y -> .<abs_float .~x < abs_float .~y >. )
   let normalizerf = None
@@ -34,15 +35,19 @@ module Float = struct
   let one         = run0 FloatCode.one
   let negone      = run0 FloatCode.negone
   let plus        = run2 FloatCode.plus
-  let times       = run2 FloatCode.times
   let minus       = run2 FloatCode.minus
   let uminus      = run1 FloatCode.uminus
+  let times       = run2 FloatCode.times
+  let inv         = run1 FloatCode.inv
   let div         = run2 FloatCode.div
   let better_than = maybe (fun z -> Some (run2 z)) None FloatCode.better_than
   let normalizerf = maybe (fun z -> Some z)        None FloatCode.normalizerf
   let normalizerg =      FloatCode.normalizerg (* this is a cheat? *)
 end
 
+(* And here is where things really get interesting.  Explicitly using the
+  Direct and Code versions, we weave them together to create a simplifying
+  partial evaluator. *)
 module FloatStaged = struct
   type v          = FloatCode.v
   type kind       = FloatCode.kind
@@ -52,6 +57,9 @@ module FloatStaged = struct
   let one       = Staged.of_atom Float.one
   let plus  a b = monoid Float.zero Float.plus FloatCode.plus (a,b)
   let uminus    = Staged.unary Float.uminus FloatCode.uminus
+  let minus     = inverse Float.zero uminus Float.minus FloatCode.minus
   let times     = ring Float.zero Float.one Float.negone uminus Float.times FloatCode.times
+  let inv       = Staged.unary Float.inv FloatCode.inv
+  let div       = inverse Float.one inv Float.div FloatCode.div
 end
 
