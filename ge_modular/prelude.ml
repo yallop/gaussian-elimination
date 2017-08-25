c5ef10eca973cc4e801afc62e105015c180bc1f7
Jacques Carette <carette@mcmaster.ca>
2017-08-24 22:20:07 -0400

    maybe_apply2, to unwrap 2 things to perhaps apply

--- ge_modular/prelude.ml
+++ ge_modular/prelude.ml
@@ -1,3 +1,7 @@
 let maybe f c = function
   | Some x -> f x
   | None   -> c
+
+let maybe_apply2 f a b = match (a,b) with
+  | Some aa , Some bb -> Some (fun x y -> f aa bb x y)
+  | _ , _ -> None
--- ge_modular/prelude.mli
+++ ge_modular/prelude.mli
@@ -1,1 +1,2 @@
 val maybe : ('a -> 'b) -> 'b -> 'a option -> 'b
+val maybe_apply2 : ('a -> 'b -> 'c -> 'd -> 'e) -> 'a option -> 'b option -> ('c -> 'd -> 'e) option
